c13253d575d618a2a523fc1d409d70f232e00cc8
samuelnerat'samuelneratSamuel Nerat <samuelfavour2013@gmail.com>
2024-11-09 22:39:09 +0100

    update on time input

--- src/components/TimerInput.jsx
+++ src/components/TimerInput.jsx
@@ -1,31 +1,62 @@
-import React from 'react';
+// import React from 'react';
 
-const TimerInput = ({ onSetTime, setHours, setMinutes, setSeconds }) => {
+// const TimerInput = ({ onSetTime, setHours, setMinutes, setSeconds }) => {
+//   return (
+//     <div className="flex space-x-2 mb-4">
+//       <input
+//         type="number"
+//         min="0"
+//         className="w-16 p-2 border rounded"
+//         onChange={(e) => setHours(e.target.value)}
+//         placeholder="HH"
+//       />
+//       <input
+//         type="number"
+//         min="0"
+//         className="w-16 p-2 border rounded"
+//         onChange={(e) => setMinutes(e.target.value)}
+//         placeholder="MM"
+//       />
+//       <input
+//         type="number"
+//         min="0"
+//         className="w-16 p-2 border rounded"
+//         onChange={(e) => setSeconds(e.target.value)}
+//         placeholder="SS"
+//       />
+//     </div>
+//   );
+// };
+
+// export default TimerInput;
+
+
+
+const TimerInput = ({ setHours, setMinutes, setSeconds }) => {
   return (
     <div className="flex space-x-2 mb-4">
       <input
         type="number"
         min="0"
         className="w-16 p-2 border rounded"
-        onChange={(e) => setHours(e.target.value)}
+        onChange={(e) => setHours(parseInt(e.target.value) || 0)}
         placeholder="HH"
       />
       <input
         type="number"
         min="0"
         className="w-16 p-2 border rounded"
-        onChange={(e) => setMinutes(e.target.value)}
+        onChange={(e) => setMinutes(parseInt(e.target.value) || 0)}
         placeholder="MM"
       />
       <input
         type="number"
         min="0"
         className="w-16 p-2 border rounded"
-        onChange={(e) => setSeconds(e.target.value)}
+        onChange={(e) => setSeconds(parseInt(e.target.value) || 0)}
         placeholder="SS"
       />
     </div>
   );
 };
-
-export default TimerInput;
+export default TimerInput;  
